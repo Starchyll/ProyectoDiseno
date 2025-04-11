27ba71730b052d6a681899a33c6b6939d443e5b8
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-04-10 17:21:35 -0700

    Creación de subsistema seleccion de asiento mockeado

--- RutAppDTO/src/main/java/itson/rutappdto/CamionDTO.java
+++ RutAppDTO/src/main/java/itson/rutappdto/CamionDTO.java
@@ -8,18 +8,13 @@ import java.util.List;
  * @author chris
  */
 public class CamionDTO {
-    private Long idCamion;
+
     private String numeroCamion;
     private List<AsientoDTO> listaAsiento;
 
-    public CamionDTO(Long idCamion, String matricula, List<AsientoDTO> listaAsiento) {
-        this.idCamion = idCamion;
+    public CamionDTO(String matricula, List<AsientoDTO> listaAsiento) {
         this.numeroCamion = matricula;
         this.listaAsiento = listaAsiento;
-    }
-
-    public Long getIdCamion() {
-        return idCamion;
     }
 
     public String getNumeroCamion() {
@@ -29,6 +24,5 @@ public class CamionDTO {
     public List<AsientoDTO> getListaAsiento() {
         return listaAsiento;
     }
-    
-    
+
 }
